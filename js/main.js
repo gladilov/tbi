264d714df21a5e363e255215d0761b0fe34a5003
Alex Gladilov <a.gladilov@yandex.ru>
2016-04-23 05:27:54 +0300

    Change html/css/js

--- js/main.js
+++ js/main.js
@@ -1,81 +1,91 @@
-
-$(function() {
+( function( $, undefined ) {
 
   $( document ).bind( "mobileinit", function() {
     $.mobile.allowCrossDomainPages = true;
   });
 
-	$('.tabs__caption').on('click', 'li:not(.tabs__content_active)', function() {
-		$(this).addClass('tabs__item_active')
-				.siblings()
-				.removeClass('tabs__item_active')
-				.closest('.tabs')
-				.find('.tabs__content')
-				.removeClass('tabs__content_active')
-				.eq( $(this).index() ).addClass('tabs__content_active');
-	});
+  $( document ).bind( "pagecreate", function( e ) {
   
-  $('#signin form').submit(function(e){
-    e.preventDefault();
-    //return false;
+    $('.tabs__caption').on('click', 'li:not(.tabs__content_active)', function() {
+      $(this).addClass('tabs__item_active')
+          .siblings()
+          .removeClass('tabs__item_active')
+          .closest('.tabs')
+          .find('.tabs__content')
+          .removeClass('tabs__content_active')
+          .eq( $(this).index() ).addClass('tabs__content_active');
+    });
     
-    //$.mobile.changePage("idea.html");
-    $(':mobile-pagecontainer').pagecontainer('change', 'idea.html');
-  });
-  
-  
-  
-  // Idea add
-  $('#idea-add form').on('change', '.checkempty', function() {
-      if ($(this).val()) {
-        $(this).next().css('z-index', '999');
-      } else {
-        $(this).next().css('z-index', '-10');
-      }
-  });
-  
-  
-  // Idea make
-  if ($('#idea-make').length) {
-    $('#spros').slider({
-      value : 0,
-      min : 0,
-      max : 10,
-      step : 1,
-      slide: function( event, ui ) {
-        if(ui.value>0){
-        $("#spros span.ui-slider-handle").html("<p>"+ui.value+"</p>").addClass("notemptyslider")}else {$("#spros span.ui-slider-handle" ).html('').removeClass("notemptyslider")}
-        if(ui.value == 10){
-        $("#spros span.ui-slider-handle p").css({"left":"0px"})}else {$("#spros span.ui-slider-handle p").css({"left":"10px"})}
-      }
+    $('#signin form').submit(function(e){
+      e.preventDefault();
+      
+      $(':mobile-pagecontainer').pagecontainer('change', 'idea.html');
+    });
+    
+    
+    
+    // Idea add
+    $('#ideaaddform .form-item').on('click', 'a.form-item-add', function(e) {
+        e.preventDefault();
+        
+        var fieldItem = $(this).parents('.form-item'),
+            controlGroup = fieldItem.find('.controlgroup'),
+            fieldNew = controlGroup.find('.ui-input-text:first-child').find('input').clone(),
+            num = fieldItem.find('.multiple').length;
+        
+        fieldNew.attr({'id': fieldNew.attr('id') + '-' + (num + 1), 'name': fieldNew.attr('name') + '-' + (num + 1)}).val('');
+        
+        controlGroup.controlgroup('container').append(fieldNew);
+        fieldNew.textinput();
+        controlGroup.controlgroup('refresh');
+    });
+    $('#ideaaddform').submit(function(e){
+      e.preventDefault();
+      
+      //console.log($(this).serialize());
+      //console.log(JSON.stringify($(this).serialize()));
+      
+      $.ajax({
+        type: "POST",
+        url: "http://y-b-i.com/api/idea.php",
+        data: $(this).serialize(),
+      })
+      .done(function(data, textStatus, jqXHR) {
+          $.mobile.loading('show');
+          
+          data = $.parseJSON(data);
+          
+          if (data.iid && data.iid != 0) {
+            console.log("Идея сохранена.");
+            
+            var div = $('<div/>', {
+              'data-iid': data.iid
+            }).appendTo('body');
+            
+            $(':mobile-pagecontainer').pagecontainer('change', 'ideastep-1.html');
+          }
+
+          
+          $.mobile.loading('hide');
+      })
+      .fail(function(jqXHR, textStatus, errorThrown) {
+        console.log('fail');
+        console.log(jqXHR.responseText);
+      });
+      
+    });
+    
+    
+    // Idea step 1
+    $('#ideastep1form').submit(function(e){
+      e.preventDefault();
+      $(':mobile-pagecontainer').pagecontainer('change', 'ideastep-2.html');
+    });
+    // Idea step 2
+    $('#ideastep2form').submit(function(e){
+      e.preventDefault();
+      $(':mobile-pagecontainer').pagecontainer('change', 'ideastep-3.html');
     });
 
-    $('#success').slider({
-      value : 0,
-      min : 0,
-      max : 10,
-      step : 1,
-      slide: function( event, ui ) {
-        if(ui.value>0){
-        $("#success span.ui-slider-handle").html("<p>"+ui.value+"</p>").addClass("notemptyslider")}else {$("#success span.ui-slider-handle" ).html('').removeClass("notemptyslider")}
-        if(ui.value == 10){
-        $("#success span.ui-slider-handle p").css({"left":"0px"})}else {$("#success span.ui-slider-handle p").css({"left":"10px"})}
-      }
-    });
-
-    $('#profit').slider({
-      value : 0,
-      min : 0,
-      max : 10,
-      step : 1,
-      slide: function( event, ui ) {
-        if(ui.value>0){
-        $("#profit span.ui-slider-handle").html("<p>"+ui.value+"</p>").addClass("notemptyslider")}else {$("#profit span.ui-slider-handle" ).html('').removeClass("notemptyslider")}
-        if(ui.value == 10){
-        $("#profit span.ui-slider-handle p").css({"left":"0px"})}else {$("#profit span.ui-slider-handle p").css({"left":"10px"})}
-      }
-    });
-  }
-  
-  
-});
+  });
+})( jQuery );
