21b9a56bd18a778a6fef7b7513d3b20d15c40624
Alex Gladilov <a.gladilov@yandex.ru>
2016-05-09 20:29:28 +0300

    Change html/js/css

--- js/main.js
+++ js/main.js
@@ -17,27 +17,36 @@
     jqmReadyDeferred.resolve();
   });
 
-  $.when(deviceReadyDeferred, jqmReadyDeferred).then(doWhenBothFrameworksLoaded);
-  //$.when(jqmReadyDeferred).then(doWhenBothFrameworksLoaded);
+  //$.when(deviceReadyDeferred, jqmReadyDeferred).then(doWhenBothFrameworksLoaded);
+  $.when(jqmReadyDeferred).then(doWhenBothFrameworksLoaded);
 
   function doWhenBothFrameworksLoaded() {
     // StatusBar
-    StatusBar.overlaysWebView(false);
+    /*StatusBar.overlaysWebView(false);
     if (cordova.platformId == 'android') {
       StatusBar.backgroundColorByHexString("#16635D");
-    }
+    }*/
     
-    console.log('doWhenBothFrameworksLoaded');
     
-    $(document).on('pagecreate', function(e) {
-    console.log('pagecreate');
+    /*$(document).on('pagecontainercreate', function(event, ui) {
+      console.log('pagecontainercreate');
+    });
+    $(document).on('pagecontainerload', function(event, ui) {
+      console.log('pagecontainerload');
+    });
+    $(document).one('pagecontainershow', function(event, ui) {
+      console.log('pagecontainershow');
+    });*/
+    
+    //$(document).on('pagecontainercreate', function(e) {
+    //$(document).on('pagecreate', function(e) {
       
       // Panel swipe
-      if ($('#left-panel').length) {
-        $(document).on('swipeleft swiperight', function( e ) {
-          // We check if there is no open panel on the page because otherwise
-          // a swipe to close the left panel would also open the right panel (and v.v.).
-          // We do this by checking the data that the framework stores on the page element (panel: open).
+      $(document).on('swipeleft swiperight', function( e ) {
+        // We check if there is no open panel on the page because otherwise
+        // a swipe to close the left panel would also open the right panel (and v.v.).
+        // We do this by checking the data that the framework stores on the page element (panel: open).
+        if ($('#left-panel').length) {
           if ( $( ".ui-page-active" ).jqmData( "panel" ) !== "open" ) {
             if ( e.type === "swiperight" ) {
               $( "#left-panel" ).panel( "open" );
@@ -45,25 +54,13 @@
               $( "#left-panel" ).panel( "close" );
             }
           }
-        });
-      }
+        }
+      });
       
     
       // Form validate
       //$("form").validate();
-      
-    
-      // Tabs 
-      $('.tabs__caption').on('click', 'li:not(.tabs__content_active)', function() {
-        $(this).addClass('tabs__item_active')
-            .siblings()
-            .removeClass('tabs__item_active')
-            .closest('.tabs')
-            .find('.tabs__content')
-            .removeClass('tabs__content_active')
-            .eq( $(this).index() ).addClass('tabs__content_active');
-      });
-      
+
       
       // Form-item multiple
       $('.form-item-multiple').on('click', 'a.form-item-add', function(e) {
@@ -86,7 +83,7 @@
       $('#signin-form').submit(function(e){
         e.preventDefault();
         
-        if ($("#signin-form:has(.required.error)").length == 0)
+        //if ($("#signin-form:has(.required.error)").length == 0)
           $(':mobile-pagecontainer').pagecontainer('change', 'idea.html');
       });
       
@@ -108,40 +105,33 @@
           crossDomain: true,
         })
         .done(function(data, textStatus, jqXHR) {
-            //data = $.parseJSON(data);
-            //console.log('done');
-            //console.log(data);
-            
-            navigator.notification.alert(
-              'Вы успешно зарегистрированы! Теперь Вы можете приступить к добавлению идей.',
-              alertCallback,
-              'Регистрация',
-              'Закрыть'
-            );
+          //data = $.parseJSON(data);
+          //console.log('done');
+          //console.log(data);
+          
+          navigator.notification.alert(
+            'Вы успешно зарегистрированы! Теперь Вы можете приступить к добавлению идей.',
+            null,
+            'Регистрация',
+            'Закрыть'
+          );
         })
         .fail(function(jqXHR, textStatus, errorThrown) {
-          data = $.parseJSON(jqXHR.responseText);
+          /*data = $.parseJSON(jqXHR.responseText);
           console.log('fail');
-          console.log(data);
+          console.log(data);*/
+          
+          navigator.notification.alert(
+            'Оштбка регистрации.',
+            null,
+            'Регистрация',
+            'Закрыть'
+          );
         });
       });
 
-      
-      
-      // Idea step 1
-      $('#ideastep1form').submit(function(e){
-        e.preventDefault();
-        $(':mobile-pagecontainer').pagecontainer('change', 'ideastep-2.html');
-      });
-      
-      
-      // Idea step 2
-      $('#ideastep2form').submit(function(e){
-        e.preventDefault();
-        $(':mobile-pagecontainer').pagecontainer('change', 'ideastep-3.html');
-      });
     
-    });
+    //});
   }
 
   
