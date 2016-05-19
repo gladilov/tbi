3a067a475e9a532497d06e2d82abbf61547c6334
Alex Gladilov <a.gladilov@yandex.ru>
2016-05-19 18:26:18 +0300

    Change js

--- js/main.js
+++ js/main.js
@@ -11,16 +11,6 @@
 
   function deviceReady() {
     deviceReadyDeferred.resolve();
-    
-    if (app) {
-      console.log(device);
-      navigator.notification.alert(
-        'Test notification',
-        null,
-        'Test',
-        'Закрыть'
-      );
-    }
   }
 
   $(document).on('mobileinit', function() {
@@ -94,8 +84,8 @@
         var $thisForm = $(this);
         
         if ($("#signin-form:has(input.required.error)").length == 0) {
-          $pageLoader.find().show();
           $('.message', $pageLoader).text('Входим...');
+          if (app) StatusBar.hide();
           $pageLoader.fadeIn(150);
           
           var request = $.ajax({
@@ -112,76 +102,76 @@
           });
           
           var state = request.state();
-          console.log(state);
           
           request.done(function(data, textStatus, jqXHR) {
             // Success:
             if (data.status == 'success') {
               setTimeout(function() {
-                  $pageLoader.fadeOut(150);
-                  $('.message', $pageLoader).empty();
-                  
-                  $thisForm.find('.ui-input-text > input').removeClass('error');
-                  $(':mobile-pagecontainer').pagecontainer('change', 'idea.html');
-                }, 2000
-              );
+                $pageLoader.fadeOut(150);
+                $('.message', $pageLoader).empty();
+                if (app) StatusBar.show();
+                
+                $thisForm.find('.ui-input-text > input').removeClass('error');
+                $(':mobile-pagecontainer').pagecontainer('change', 'idea.html', {reloadPage: true});
+              }, 2000);
             }
             // Error:
             else if (data.status == 'error') {
               setTimeout(function() {
-                  $pageLoader.fadeOut(150);
-                  $('.message', $pageLoader).empty();
-                  
-                  $thisForm.find('.ui-input-text > input').addClass('error');
+                $pageLoader.fadeOut(150);
+                $('.message', $pageLoader).empty();
+                if (app) StatusBar.show();
+                
+                $thisForm.find('.ui-input-text > input').addClass('error');
 
-                  if (app) {
-                    navigator.notification.alert(
-                      data.message,
-                      null,
-                      'Авторизация',
-                      'Закрыть'
-                    );
-                  }
-                  else {
-                    console.log('Ошибка авторизации (data.message: "' + data.message + '")');
-                  }
-                }, 2000
-              );
+                if (app) {
+                  navigator.notification.alert(
+                    data.message,
+                    null,
+                    'Авторизация',
+                    'Закрыть'
+                  );
+                }
+                else {
+                  console.log('Ошибка авторизации (data.message: "' + data.message + '")');
+                }
+              }, 2000);
             }
           });
           
           request.fail(function(jqXHR, textStatus, errorThrown) {
             setTimeout(function() {
-                $pageLoader.hide();
-                
-                if (textStatus == 'timeout') {
-                  if (app) {
-                    navigator.notification.alert(
-                      'Ошибка авторизации - сервер не ответил в отведенное время. Попробуйте выполнить запрос позже.',
-                      null,
-                      'Авторизация',
-                      'Закрыть'
-                    );
-                  }
-                  else {
-                    console.log('Ошибка авторизации - сервер не ответил в отведенное время. Попробуйте выполнить запрос позже.');
-                  }
+              $pageLoader.fadeOut(150);
+              $('.message', $pageLoader).empty();
+              if (app) StatusBar.show();
+              
+              if (textStatus == 'timeout') {
+                if (app) {
+                  navigator.notification.alert(
+                    'Ошибка авторизации - сервер не ответил в отведенное время. Попробуйте выполнить запрос позже.',
+                    null,
+                    'Авторизация',
+                    'Закрыть'
+                  );
                 }
                 else {
-                  if (app) {
-                    navigator.notification.alert(
-                      'Ошибка авторизации.',
-                      null,
-                      'Авторизация',
-                      'Закрыть'
-                    );
-                  }
-                  else {
-                    console.log('Ошибка авторизации (textStatus: "' + textStatus + '").');
-                  }
+                  console.log('Ошибка авторизации - сервер не ответил в отведенное время. Попробуйте выполнить запрос позже.');
                 }
-              }, 2000
-            );
+              }
+              else {
+                if (app) {
+                  navigator.notification.alert(
+                    'Ошибка авторизации.',
+                    null,
+                    'Авторизация',
+                    'Закрыть'
+                  );
+                }
+                else {
+                  console.log('Ошибка авторизации (textStatus: "' + textStatus + '").');
+                }
+              }
+            }, 2000);
           });
         }
       });
@@ -194,51 +184,100 @@
         var $thisForm = $(this);
         
         if ($("#signup-form:has(input.required.error)").length == 0) {
-          $.ajax({
+          $('.message', $pageLoader).text('Регистрируем...');
+          if (app) StatusBar.hide();
+          $pageLoader.fadeIn(150);
+          
+          var request = $.ajax({
             type: 'GET',
             dataType: 'jsonp',
             jsonpCallback: 'userCreate',
             contentType: "application/json; charset=utf-8",
             url: 'http://y-b-i.com/api/user.php',
             data: {"method": "post", "data": $(this).serialize()},
+            timeout: 8000,
             cache: false,
             async: true,
             crossDomain: true,
-          })
-          .done(function(data, textStatus, jqXHR) {
-            console.log('done');
-            console.log(data);
-            
+          });
+          
+          var state = request.state();
+          
+          request.done(function(data, textStatus, jqXHR) {
             // Success:
             if (data.status == 'success') {
-              $thisForm.find('.ui-input-text > input').removeClass('error');
-              
-              navigator.notification.alert(
-                data.message,
-                function () { $(':mobile-pagecontainer').pagecontainer('change', 'idea.html'); },
-                'Регистрация',
-                'Закрыть'
-              );
+              setTimeout(function() {
+                $pageLoader.fadeOut(150);
+                $('.message', $pageLoader).empty();
+                if (app) StatusBar.show();
+                
+                $thisForm.find('.ui-input-text > input').removeClass('error');
+                
+                navigator.notification.alert(
+                  data.message,
+                  function () { $(':mobile-pagecontainer').pagecontainer('change', 'idea.html', {reloadPage: true}); },
+                  'Регистрация',
+                  'Закрыть'
+                );
+              }, 2000);
             }
             // Error:
             else if (data.status == 'error') {
-              $thisForm.find('.ui-input-text > input').addClass('error');
+              setTimeout(function() {
+                $pageLoader.fadeOut(150);
+                $('.message', $pageLoader).empty();
+                if (app) StatusBar.show();
+                  
+                $thisForm.find('.ui-input-text > input').addClass('error');
 
-              navigator.notification.alert(
-                data.message,
-                null,
-                'Регистрация',
-                'Закрыть'
-              );
+                if (app) {
+                  navigator.notification.alert(
+                    data.message,
+                    null,
+                    'Регистрация',
+                    'Закрыть'
+                  );
+                }
+                else {
+                  console.log('Ошибка регистрации (data.message: "' + data.message + '")');
+                }
+              }, 2000);
             }
-          })
-          .fail(function(jqXHR, textStatus, errorThrown) {
-            navigator.notification.alert(
-              'Ошибка регистрации.',
-              null,
-              'Регистрация',
-              'Закрыть'
-            );
+          });
+          
+          request.fail(function(jqXHR, textStatus, errorThrown) {
+            setTimeout(function() {
+              $pageLoader.fadeOut(150);
+              $('.message', $pageLoader).empty();
+              if (app) StatusBar.show();
+              
+              if (textStatus == 'timeout') {
+                if (app) {
+                  navigator.notification.alert(
+                    'Ошибка регистрации - сервер не ответил в отведенное время. Попробуйте выполнить запрос позже.',
+                    null,
+                    'Регистрация',
+                    'Закрыть'
+                  );
+                }
+                else {
+                  console.log('Ошибка регистрации - сервер не ответил в отведенное время. Попробуйте выполнить запрос позже.');
+                }
+              }
+              else {
+                if (app) {
+                  navigator.notification.alert(
+                    'Ошибка регистрации. Попробуйте выполнить запрос повторно.',
+                    null,
+                    'Регистрация',
+                    'Закрыть'
+                  );
+                }
+                else {
+                  console.log('Ошибка регистрации (textStatus: "' + textStatus + '").');
+                }
+              }
+            }, 2000);
           });
         }
       });
