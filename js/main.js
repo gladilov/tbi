23f04915a1bbe0fc0ca50667e6be27ee7c722744
Alex Gladilov <a.gladilov@yandex.ru>
2016-07-04 22:19:42 +0300

    Change html/js and config.xml

--- js/main.js
+++ js/main.js
@@ -7,7 +7,7 @@
       uid = 0,
       userAuthorized = false,
       app = document.URL.indexOf( 'http://' ) === -1 && document.URL.indexOf( 'https://' ) === -1,
-      appVersion = '0.7.3';
+      appVersion = '0.7.4';
 
   // Namespace storage
   var ybi = $.initNamespaceStorage('ybi');
